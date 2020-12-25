0e5142727f76dabffc944ca67ea4988d50497ce3
DreamakerDimas <dart.dimas@gmail.com>
2020-12-26 00:42:40 +0200

    Init

---
+++ server/src/server/controllers/queries/userQueries.js
@@ -0,0 +1,42 @@
+const db = require('../../models/index');
+const NotFound = require('../../errors/UserNotFoundError');
+const ServerError = require('../../errors/ServerError');
+const bcrypt = require('bcrypt');
+const { date } = require('yup');
+
+module.exports.updateUser = async (data, userId, transaction) => {
+  const [updatedCount, [updatedUser]] = await db.Users.update(data, {
+    where: { id: userId },
+    returning: true,
+    transaction,
+  });
+  if (updatedCount !== 1) {
+    throw new ServerError('cannot update user');
+  }
+  return updatedUser.dataValues;
+};
+
+module.exports.findUser = async (predicate, transaction) => {
+  const result = await db.Users.findOne({ where: predicate, transaction });
+  if (!result) {
+    throw new NotFound('user with this data didn`t exist');
+  } else {
+    return result.get({ plain: true });
+  }
+};
+
+module.exports.userCreation = async (data) => {
+  const newUser = await db.Users.create(data); //Something wrong here
+  if (!newUser) {
+    throw new ServerError('server error on user creation');
+  } else {
+    return newUser.get({ plain: true });
+  }
+};
+
+module.exports.passwordCompare = async (pass1, pass2) => {
+  const passwordCompare = await bcrypt.compare(pass1, pass2);
+  if (!passwordCompare) {
+    throw new NotFound('Wrong password');
+  }
+};

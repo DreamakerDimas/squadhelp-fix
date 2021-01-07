259b0070155d42ae6224ceb23392d80f50b515eb
DreamakerDimas <dart.dimas@gmail.com>
2021-01-07 15:37:05 +0200

    (front) removed unused imports

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,11 +4,7 @@ import styles from './Header.module.sass';
 import { connect } from 'react-redux';
 import { Link, withRouter } from 'react-router-dom';
 import CONSTANTS from '../../constants';
-import {
-  getUserAction,
-  clearUserStore,
-  headerRequest,
-} from '../../actions/actionCreator';
+import { clearUserStore, headerRequest } from '../../actions/actionCreator';
 
 class Header extends React.Component {
   componentDidMount() {

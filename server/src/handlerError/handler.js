eefd577924843b7d5df31f9426411049c6a8d17c
DreamakerDimas <dart.dimas@gmail.com>
2021-02-01 16:50:17 +0200

    back error fix

--- server/src/handlerError/handler.js
+++ server/src/handlerError/handler.js
@@ -5,13 +5,21 @@ module.exports = (err, req, res, next) => {
   logWrite(err);
   if (
     err.message ===
-      'new row for relation "Banks" violates check constraint "Banks_balance_ck"' ||
-    err.message ===
-      'new row for relation "Users" violates check constraint "Users_balance_ck"'
+    'new row for relation "Users" violates check constraint "Users_balance_ck"'
   ) {
     err.message = 'Not Enough money';
     err.code = 406;
   }
+
+  if (
+    err.message ===
+    'new row for relation "Banks" violates check constraint "Banks_balance_ck"'
+  ) {
+    err.message =
+      'Sorry, but currently we have some problems. Please try again later.';
+    err.code = 406;
+  }
+
   if (!err.message || !err.code) {
     res.status(500).send('Server Error');
   } else {
